9c2f66615d71a69a02f0ba8aa3af8f25d3925cf7
PrakashDevX <Prakashclever43@gmail.com>
2026-03-08 16:56:50 +0530

    Update server.js

--- prakash-text2image-gpt/backend/server.js
+++ prakash-text2image-gpt/backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const app = express();
@@ -18,25 +17,30 @@ app.use(express.json());
 app.post("/generate", async (req, res) => {
     try {
         const { prompt } = req.body;
+
         const response = await fetch(
             "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
             {
                 method: "POST",
                 headers: {
                     Authorization: `Bearer ${process.env.HF_API_KEY}`,
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
+                    "x-wait-for-model": "true"
                 },
                 body: JSON.stringify({ inputs: prompt })
             }
         );
+
         if (!response.ok) {
             const errText = await response.text();
             console.log("HF Error:", errText);
             return res.status(500).send(errText);
         }
+
         const buffer = Buffer.from(await response.arrayBuffer());
         res.setHeader("Content-Type", "image/png");
         res.send(buffer);
+
     } catch (err) {
         console.error("Server Error:", err);
         res.status(500).send("Backend error");
