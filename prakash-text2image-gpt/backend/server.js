53458dadf60a5f2f5d6c570cedac1f5e68a59f5c
PrakashDevX <Prakashclever43@gmail.com>
2026-03-08 16:03:16 +0530

    Update CORS2 server.js

--- prakash-text2image-gpt/backend/server.js
+++ prakash-text2image-gpt/backend/server.js
@@ -5,18 +5,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://your-app-name.vercel.app"
+    "https://rp-vision-ai.vercel.app"
   ]
 }));
+
 app.use(express.json());
 
 app.post("/generate", async (req, res) => {
     try {
         const { prompt } = req.body;
-
         const response = await fetch(
             "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0",
             {
@@ -25,23 +26,17 @@ app.post("/generate", async (req, res) => {
                     Authorization: `Bearer ${process.env.HF_API_KEY}`,
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({
-                    inputs: prompt
-                })
+                body: JSON.stringify({ inputs: prompt })
             }
         );
-
         if (!response.ok) {
             const errText = await response.text();
             console.log("HF Error:", errText);
             return res.status(500).send(errText);
         }
-
         const buffer = Buffer.from(await response.arrayBuffer());
-
         res.setHeader("Content-Type", "image/png");
         res.send(buffer);
-
     } catch (err) {
         console.error("Server Error:", err);
         res.status(500).send("Backend error");
